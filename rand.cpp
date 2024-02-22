710aa80ef0b3d4468e1cfd11152926eb45604693
Amelka T <amelia.tosik2007@outlook.com>
2024-02-22 20:33:07 +0100

    tab

--- rand.cpp
+++ rand.cpp
@@ -8,61 +8,50 @@ using namespace std;
 int main() {
     constexpr int rozmiar = 12;
     int tab[rozmiar];
-    
+
     wypelnij(tab, rozmiar);
     wyswietl(tab, rozmiar);
-    cout<<endl;
+
+    cout << endl;
+
     sort_babelkowe(tab, rozmiar);
     wyswietl(tab, rozmiar);
-
-    cout<<endl;
-
-    wypelnij(tab, rozmiar);
-    wyswietl(tab, rozmiar);
-    cout<<endl;
+ 
+    cout << endl << endl;
+ 
     sort_scalanie(tab, 0, rozmiar);
     wyswietl(tab, rozmiar);
-
-    cout<<endl;
-
-    wypelnij(tab, rozmiar);
+ 
+    cout << endl << endl;
+ 
+    sort_wstrzasanie(tab, rozmiar);
     wyswietl(tab, rozmiar);
-    cout<<endl;
-    sort_wstrzasanie(tab, 0, rozmiar);
-    wyswietl(tab, rozmiar);
-    
-    cout<<endl;
 
-    wypelnij(tab, rozmiar);
-    wyswietl(tab, rozmiar);
-    cout<<endl;
+    cout << endl << endl;
+ 
     sort_szybkie(tab, 0, rozmiar);
     wyswietl(tab, rozmiar);
-
-    cout<<endl;
-
-    wypelnij(tab, rozmiar);
+ 
+    cout << endl << endl;
+ 
+    sort_wstawianie(tab, rozmiar);
     wyswietl(tab, rozmiar);
-    cout<<endl;
-    sort_wstawianie(tab, 0, rozmiar);
-    wyswietl(tab, rozmiar);
-
-
-
+ 
+    cout << endl << endl;
+ 
     int liczba1 = 10;
     int liczba2 = 15;
-    
     int tab2[rozmiar] = {1, 4, 8, 9, 10, 13, 14, 17, 19, 20, 23, 24};
-    
-    cout<<wysz_liniowe(tab2, rozmiar, liczba1);
-    cout<<wysz_liniowe(tab2, rozmiar, liczba2);
 
-    cout<<wysz_binarne(tab2, 0, rozmiar, liczba1);
-    cout<<wysz_binarne(tab2, 0, rozmiar, liczba2);
-
-    cout<<wysz_interpolacyjne(tab2, 0, rozmiar, liczba1);
-    cout<<wysz_interpolacyjne(tab2, 0, rozmiar, liczba2);
-
-    cout<<wysz_skokowe(tab2, rozmiar, liczba1);
-    cout<<wysz_skokowe(tab2, rozmiar, liczba2);
+    cout << wysz_liniowe(tab2, rozmiar, liczba1) << " ";
+    cout << wysz_liniowe(tab2, rozmiar, liczba2) << endl;
+ 
+    cout << wysz_binarne(tab2, 0, rozmiar, liczba1) << " ";
+    cout << wysz_binarne(tab2, 0, rozmiar, liczba2) << endl;
+ 
+    cout << wysz_interpolacyjne(tab2, 0, rozmiar, liczba1) << " ";
+    cout << wysz_interpolacyjne(tab2, 0, rozmiar, liczba2) << endl;
+ 
+    cout << wysz_skokowe(tab2, rozmiar, liczba1) << " ";
+    cout << wysz_skokowe(tab2, rozmiar, liczba2) << endl;
 };
